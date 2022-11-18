76d8ad35bac8103dac8f1e9d1d7889e934cddf30
sphuang28 <sp.huang0628@gmail.com>
2022-11-18 14:18:28 +0800

    9. Palindrome Number

--- no9.py
+++ no9.py
@@ -2,9 +2,9 @@
 n=str(input("請輸入任意數字:"))
 
 if int(n)<0 or int(n)/10==0 or (int(n)/10==0):
-    print("不是回文數")
+    print("不是回文")
 if n == n[::-1] :
-    print("是回文數")
+    print("是回文")
 else :
     print("不是回文數")
 
